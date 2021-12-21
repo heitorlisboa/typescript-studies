5af2cf5125e38a2bfa001aee1a56c535a09fbbdc
Heitor Lisboa <89428328+heitorlisboa@users.noreply.github.com>
2021-12-21 18:49:57 -0300

    Comentário explicativo

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,4 +1,5 @@
 export default class Negociacao {
+  // Utilizar as próprias declarações (com public/private/protected) dentro dos parâmetros faz com que o typescript compile o código tanto recebendo os parâmetros como já declarando as propriedades
   constructor(
     private _data: Date,
     // readonly faz com que a propriedade só possa ser lida mas não ter nenhum valor atribuido porém ainda é possível utilizar seus métodos, por isso a variável data não é declarada como readonly mas sim como private, sendo depois criado um getter que retorna uma cópia
