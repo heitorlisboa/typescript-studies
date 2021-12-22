095b9b1ee65a7cc9325357eb33efdda89630f2f5
Heitor Lisboa <89428328+heitorlisboa@users.noreply.github.com>
2021-12-22 11:53:55 -0300

    Compilei o código

---
+++ app/dist/js/decorators/loga-tempo-execucao.js
@@ -0,0 +1,13 @@
+export default function logaTempoExecucao() {
+    return function (target, propertyKey, descriptor) {
+        const metodoOriginal = descriptor.value;
+        descriptor.value = function (...args) {
+            const t1 = performance.now();
+            const retorno = metodoOriginal.apply(this, args);
+            const t2 = performance.now();
+            console.log(`${propertyKey}, execução levou ${(t2 - t1) / 1000} segundos`);
+            return retorno;
+        };
+        return descriptor;
+    };
+}
