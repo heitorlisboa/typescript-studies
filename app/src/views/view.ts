8f713e9beb9e58c94dccefea4e085a751a7aa6a9
Heitor Lisboa <89428328+heitorlisboa@users.noreply.github.com>
2021-12-22 12:22:32 -0300

    Implementação de um decorator de performance

--- app/src/views/view.ts
+++ app/src/views/view.ts
@@ -1,5 +1,8 @@
 // O <T> é conhecido como generic e serve pra manter um mesmo tipo em diferentes partes da classe mas que cada filho pode apresentar um tipo diferente diferente (obs.: pode-se declarar mais de um tipo separando-os por vírgula, exemplo: <T, K, L>)
 // abstract serve para declarar uma classe abstrata, ou seja, que não pode ser instanciada, mas é somente utilizada para ser extendida por outras classes
+
+import logaTempoExecucao from "../decorators/loga-tempo-execucao.js";
+
 // protected é quase o mesmo que o private, porém deixa o atributo/método acessível aos filhos dessa classe, diferentemente do private que só permite que a classe em si acesse-o
 export default abstract class View<T> {
   protected elemento: HTMLElement;
@@ -15,6 +18,7 @@ export default abstract class View<T> {
 
   protected abstract template(model: T, ...args: any[]): string;
 
+  @logaTempoExecucao(true)
   public render(model: T, ...args: any[]): void {
     this.elemento.innerHTML = this.template(model, ...args);
   }
