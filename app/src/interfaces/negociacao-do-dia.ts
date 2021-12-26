442dd1f54abf0bc7687339290e91fcff14d946d0
Heitor Lisboa <89428328+heitorlisboa@users.noreply.github.com>
2021-12-26 14:49:01 -0300

    Alterei um comentário explicativo

--- app/src/interfaces/negociacao-do-dia.ts
+++ app/src/interfaces/negociacao-do-dia.ts
@@ -1,4 +1,6 @@
-// interface define a interface de um objeto, definindo como um objeto daquele tipo deve ser construído
+// interface define a interface de um objeto ou classe
+// No caso de objetos, define como estes devem ser construídos, devendo conter somente as propriedades definadas no interface
+// No caso de classes, define propriedades/métodos abstratos que a classe deve implementar, mas podendo implementar outras propriedades/métodos
 export default interface NegociacaoDoDia {
   vezes: number;
   montante: number;
