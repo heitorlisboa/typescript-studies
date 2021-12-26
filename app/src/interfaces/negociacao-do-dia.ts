a1970daa6389bc7d1022a6ab8df68e094ef2151a
Heitor Lisboa <89428328+heitorlisboa@users.noreply.github.com>
2021-12-26 14:34:29 -0300

    Novo comentário explicativo

--- app/src/interfaces/negociacao-do-dia.ts
+++ app/src/interfaces/negociacao-do-dia.ts
@@ -1,3 +1,4 @@
+// interface define a interface de um objeto, definindo como um objeto daquele tipo deve ser construído
 export default interface NegociacaoDoDia {
   vezes: number;
   montante: number;
